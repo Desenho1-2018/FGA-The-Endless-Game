4d40e2183b4fd5450bca4508519a8249bb2e9147
Mateus Roriz <mateusvroriz6b@gmail.com>
2018-04-22 17:26:43 -0300

    Animating the Player and creating GameObject class

--- FGA - The Endless Game/src/game/Cenario1.java
+++ FGA - The Endless Game/src/game/Cenario1.java
@@ -10,13 +10,14 @@ public class Cenario1 {
 	//private Scene  cena;
 	private Jogador jogador;
 	GameImage backGround = new GameImage("src//recursos//tiles//background_default.png");
-	
+	private GameObject wall;
 	public Cenario1(Window window){
 		
 		janela = window;
 		//cena = new Scene();
 		//cena.loadFromFile(URL.scenario("Cenario1.scn")); //carregando o arquivo com o mapa
 		jogador = new Jogador(640, 350);
+		wall = new GameObject(630,340);
 		run();
 	}
 
@@ -26,7 +27,8 @@ public class Cenario1 {
 			//cena.draw();
 			backGround.draw();
 			jogador.draw();
-			jogador.mover();
+			wall.draw();
+			jogador.mover(janela);
 			janela.update();
 			
 		}
