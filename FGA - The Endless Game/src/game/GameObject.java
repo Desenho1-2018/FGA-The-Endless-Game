9c909374f5ad4e55349c6df515fd5b7c87855423
Lucas S. <lucas.soaresouza@gmail.com>
2018-05-05 17:06:38 -0300

    Generalizing Game Objects

--- FGA - The Endless Game/src/game/GameObject.java
+++ FGA - The Endless Game/src/game/GameObject.java
@@ -3,10 +3,32 @@ package game;
 import jplay.Sprite;
 
 public class GameObject extends Sprite{
-
-	public GameObject(int x, int y){
-	super(("src//recursos//tiles//asfalto.png"), 1); // img and frame number
-	this.x = x;
-	this.y = y;
+	
+	private String gameObjectName = "";
+	private String spritePath = "";
+	
+	public GameObject(int x, int y, String spritePath){
+	
+		super((spritePath), 1); // img and frame number
+		this.spritePath = spritePath;
+		this.x = x;
+		this.y = y;
+	
 	}
+
+	public void setGameObjectName(String name) {
+		gameObjectName = name;
+	}
+	
+	public String getGameObjectName() {
+		return gameObjectName;
+	}
+	
+	public void setSpritePath(String path) {
+		spritePath = path;
+	}
+	
+
 }
+
+
