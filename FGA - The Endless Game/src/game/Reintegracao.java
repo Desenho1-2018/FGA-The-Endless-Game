70527bb33072a1804e291fee36f3b6734d509352
tayh <tayhcarvalho@gmail.com>
2018-05-05 21:51:18 -0400

    Creating runReintegration method

--- FGA - The Endless Game/src/game/Reintegracao.java
+++ FGA - The Endless Game/src/game/Reintegracao.java
@@ -6,41 +6,42 @@ import jplay.Window;
 import game.GameButton;
 
 public class Reintegracao {
-	static Window janela = new Window(800, 600);
-	static GameImage background = new GameImage("src//recursos//sprite//reint.png");
-	static Sprite numbers = new Sprite("src//recursos//sprite//continue");
-	static GameButton continue_button = new GameButton(240, 400, "src//recursos//sprite//continuebutton1.png");
 	
-	public static void main(String[] args) {
-		int count = 9;
+	public void runReintegration() {
+		Window janela = new Window(800, 600);
+		GameImage background = new GameImage("src//recursos//sprite//reint.png");
+		Sprite numbers = new Sprite("src//recursos//sprite//continue");
+		GameButton continue_button = new GameButton(240, 400, "src//recursos//sprite//continuebutton1.png");
+		
+			int count = 9;
 
+			
+			numbers.x = 350;
+			numbers.y = 230;
+			
 		
-		numbers.x = 350;
-		numbers.y = 230;
-		
-		
-		
-		for (int i = 0; i <= count; ++i) {
-			numbers.loadImage("src//recursos//sprite//continue//num" + String.valueOf(count - i) + ".png");
-			background.draw();
-			continue_button.draw();
-			numbers.draw();
-			janela.update();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Não foi possível abrir a tela de Reintegração");
-				e.printStackTrace();
+			for (int i = 0; i <= count; ++i) {
+				numbers.loadImage("src//recursos//sprite//continue//num" + String.valueOf(count - i) + ".png");
+				background.draw();
+				continue_button.draw();
+				numbers.draw();
+				janela.update();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Não foi possível abrir a tela de Reintegração");
+					e.printStackTrace();
+				}
 			}
-		}
-		
-		janela.exit();
+			
+			janela.exit();
+	}
+	
 
 
-    }
-	
-	
+
 }
+
 
 
