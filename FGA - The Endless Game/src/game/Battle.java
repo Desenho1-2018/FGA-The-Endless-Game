57e6d52992fd3d67aa2fce5a39de1f01fc7048ae
Kleber <kleberbritomoreira10@gmail.com>
2018-05-06 10:55:31 -0300

    Adding collision between note and characteres
    Signed-off-by: Kleber <kleberbritomoreira10@gmail.com>

--- FGA - The Endless Game/src/game/Battle.java
+++ FGA - The Endless Game/src/game/Battle.java
@@ -6,20 +6,20 @@ import jplay.Window;
 
 public class Battle extends Scenario {
 
-	
 	GameImage background = new GameImage(("src/recursos/sprite/UnbGama.png"));
 	
 	public Battle(Window gameWindow, String name){
+	
 	  this.window = gameWindow;
 	  this.scenarioName = name;
+	
 	}
 	
 	
 	@Override
 	public String run() {
 		
-		drawScenario();
-		
+		drawScenario();		
 		return "FinishScene";
 	
 	}
@@ -31,6 +31,7 @@ public class Battle extends Scenario {
 		Sprite professor = new Sprite("src/recursos/sprite/professor.png");
 		Sprite student = new Sprite("src/recursos/sprite/universitario.png");
 		Sprite battle = new Sprite("src/recursos/sprite/Batalha.png");
+		Sprite note = new Sprite("src/recursos/sprite/nota.png");
 		
 		battle.x = 100;
 		battle.y = 0;		
@@ -38,13 +39,23 @@ public class Battle extends Scenario {
 		professor.y = 300;		
 		student.x = 700;
 		student.y = 300;
+		note.x = 350;
+		note.y = 500;
+		
+		boolean resposta = true;
 		
 		while(true){
-			
 			backgroundBattle.draw();
 			battle.draw();
 			professor.draw();
 			student.draw();
+			note.draw();
+			
+			if(resposta == true){
+				note.moveTo(professor.x, professor.y, 0.5);
+			} else{
+				note.moveTo(student.x, student.y, 0.5);
+			}
 			window.update();
 		}
 	}
