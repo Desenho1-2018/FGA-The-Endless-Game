753562fd43953adddeb45d383b544bb449d0bddd
Mateus Roriz <mateusvroriz6b@gmail.com>
2018-05-06 15:25:38 -0300

    Checking battle scenario and reverting back to main menu

--- FGA - The Endless Game/src/game/Main.java
+++ FGA - The Endless Game/src/game/Main.java
@@ -20,7 +20,7 @@ public class Main {
 		levelStateMachine.addScenario(battle);
 		levelStateMachine.addScenario(cenario1);
 		
-		levelStateMachine.run("BattleScene");
+		levelStateMachine.run("MainMenu");
 		
 	}
 	
