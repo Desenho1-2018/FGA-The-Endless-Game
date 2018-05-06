895e0f4d63be9695cd013648c468a538144289ad
tayh <tayhcarvalho@gmail.com>
2018-05-05 22:17:30 -0400

    Implementing keyboard actions.

--- FGA - The Endless Game/src/game/Reintegracao.java
+++ FGA - The Endless Game/src/game/Reintegracao.java
@@ -1,41 +1,49 @@
 package game;
 
 import jplay.GameImage;
+import jplay.Keyboard;
 import jplay.Sprite;
 import jplay.Window;
 import game.GameButton;
 
-public class Reintegracao {
+public class Reintegracao{
 	
 	public void runReintegration() {
 		Window janela = new Window(800, 600);
 		GameImage background = new GameImage("src//recursos//sprite//reint.png");
 		Sprite numbers = new Sprite("src//recursos//sprite//continue");
 		GameButton continue_button = new GameButton(240, 400, "src//recursos//sprite//continuebutton1.png");
+		Keyboard keyboard = janela.getKeyboard();
 		
-			int count = 9;
+		int count = 9;
 
 			
-			numbers.x = 350;
-			numbers.y = 230;
+		numbers.x = 350;
+		numbers.y = 230;
 			
 		
-			for (int i = 0; i <= count; ++i) {
-				numbers.loadImage("src//recursos//sprite//continue//num" + String.valueOf(count - i) + ".png");
-				background.draw();
-				continue_button.draw();
-				numbers.draw();
-				janela.update();
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					System.out.println("Não foi possível abrir a tela de Reintegração");
-					e.printStackTrace();
-				}
+		for (int i = 0; i <= count; ++i) {
+			numbers.loadImage("src//recursos//sprite//continue//num" + String.valueOf(count - i) + ".png");
+			background.draw();
+			continue_button.draw();
+			numbers.draw();
+			janela.update();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Não foi possível abrir a tela de Reintegração");
+				e.printStackTrace();
+			}
+               if (keyboard.keyDown(Keyboard.ENTER_KEY) ) {
+            	   System.out.println("Continuei no jogo!");
+            	   janela.exit();
+               } 
+               else if (keyboard.keyDown(Keyboard.ESCAPE_KEY)){
+            	   janela.exit();
+               }
 			}
 			
-			janela.exit();
 	}
 	
 
