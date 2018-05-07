585cb33cfa354e2f377299c78eb5549785e378f0
Stefane Souza <stefanesouza04@gmail.com>
2018-05-07 00:21:31 -0300

    Showing Game Over Screen

--- FGA - The Endless Game/src/game/GameOverMenu.java
+++ FGA - The Endless Game/src/game/GameOverMenu.java
@@ -1,5 +1,53 @@
 package game;
 
-public class GameOverMenu {
+import jplay.GameImage;
+import jplay.Keyboard;
+import jplay.Window;
 
+public class GameOverMenu extends Scenario {
+
+	private String nextScenario =  null; 
+	
+	public GameOverMenu(Window gameWindow, String name) {
+		window = gameWindow;
+		scenarioName = name;
+		background = new GameImage("src//recursos//sprite//main_menu.png");
+		initializeKeyboard();
+	}
+	
+	protected void updateScenario() {
+		
+		while(nextScenario == null) {
+			drawObjects();
+			startGameAgain();
+			window.update();
+		}
+	}
+	
+	protected void initializeKeyboard() {
+		
+		if(window != null) {
+			sceneKeyboard = window.getKeyboard();
+		} else {
+			System.out.println("The keyboard needs a window to run. The window cannot be null");
+		}
+		sceneKeyboard.setBehavior(Keyboard.ENTER_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
+	
+	}
+	
+	public String runScenario() {
+		
+		updateScenario();
+		System.out.println(nextScenario);
+		return nextScenario;
+		
+	}
+	
+	private void startGameAgain() {
+		
+		if(sceneKeyboard.keyDown(Keyboard.ENTER_KEY)) {
+
+				nextScenario = "StartMenu";
+		}
+	}
 }
