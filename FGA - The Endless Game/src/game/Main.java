2a1d79ce78763e218e4c537b129657d9b4efe3e2
Kleber <kleberbritomoreira10@gmail.com>
2018-05-09 00:23:12 -0300

    Creation question about derivada and adjusting class QuestionCalculo1
    Co-authored-by: Stefane Souza <stefanesouza04@gmail.com>
    Co-authored-by: Emanuel Holanda <emanuelhbga@gmail.com>

--- FGA - The Endless Game/src/game/Main.java
+++ FGA - The Endless Game/src/game/Main.java
@@ -13,7 +13,6 @@ public class Main {
 		Introduction introScene = new Introduction(gameWindow, "IntroductionScene");
 		Battle battle = new Battle(gameWindow, "BattleScene");
 
-		Question question = new Question(gameWindow, "QuestionScene");
 
 		Level1 level1 = new Level1(gameWindow, "Level1");
 
@@ -21,10 +20,11 @@ public class Main {
 		levelStateMachine.addScenario(finishScene);
 		levelStateMachine.addScenario(introScene);
 		levelStateMachine.addScenario(battle);
-		levelStateMachine.addScenario(question);
+
 		levelStateMachine.addScenario(level1);
 
 		levelStateMachine.run("Level1");
+
 
 	}
 
