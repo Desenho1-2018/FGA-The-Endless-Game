446426bca4a0b1edd340eca8cb3fdefc44c91440
tayh <tayhcarvalho@gmail.com>
2018-05-10 11:30:01 -0300

    Add time to LevelTCC

--- FGA - The Endless Game/src/game/LevelTCC.java
+++ FGA - The Endless Game/src/game/LevelTCC.java
@@ -1,19 +1,25 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import jplay.GameImage;
+import jplay.Time;
 import jplay.Window;
 
 public class LevelTCC extends Level {
 
 	GameObject book = new GameObject(100, 200, "src//recursos//sprite//livro.gif", 1, GameObjectType.REFERENCE_ITEM);
-	GameObject pencil = new GameObject(400, 400, "src//recursos//sprite//canudol.gif", 1,GameObjectType.REFERENCE_ITEM);
+	GameObject pencil = new GameObject(400, 400, "src//recursos//sprite//canudol.gif", 1,
+			GameObjectType.REFERENCE_ITEM);
 	GameObject coffe = new GameObject(400, 100, "src//recursos//sprite//coffe.gif", 1, GameObjectType.REFERENCE_ITEM);
+	Time tempo = new Time(640, 50, false);
 
 	public LevelTCC(Window gameWindow, String name) {
 
 		this.window = gameWindow;
 		this.scenarioName = name;
-		this.background = new GameImage("src//recursos//tiles//ground_tcc.png");
+		this.background = new GameImage("src//recursos//tiles/planofundo.png");
 		addPlayer(100, 100, "src//recursos//sprite//jogador2.png", 20, window);
 		this.addSceneObjects(book);
 		this.addSceneObjects(pencil);
@@ -31,9 +37,14 @@ public class LevelTCC extends Level {
 	}
 
 	private void drawLevel() {
+		tempo.setColor(Color.WHITE);
+		tempo.setFont(new Font("sansserif", Font.TRUETYPE_FONT, 15));
+		tempo.setTime(0, 0, 30);
+
 		while (this.nextScenario == null) {
 
 			background.draw();
+			tempo.draw("Time: ");
 			book.draw();
 			pencil.draw();
 			coffe.draw();
@@ -50,21 +61,3 @@ public class LevelTCC extends Level {
 
 	}
 }
-
-// public class Level1 extends Level {
-//
-// GameImage backGround = new
-// GameImage("src//recursos//tiles//background_default.png");
-// private NPC npc;
-// private Scenario cena;
-// private GameObject parede;
-// public Level1(Window gameWindow, String name){
-//
-// this.window = gameWindow;
-// this.scenarioName = name;
-// addPlayer(640, 350, "src//recursos//sprite//jogador2.png", 20, window);
-// npc = new NPC(300,300);
-// parede = new GameObject(600,300, "src//recursos//tiles//wallextended.png", 1,
-// GameObjectType.WALL);
-// this.addSceneObjects(parede);
-// }
