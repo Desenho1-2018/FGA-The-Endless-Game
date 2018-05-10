f7bb914e88d0c69d3700bb9eb370767399bceff1
Lucas Souza <lucas.soaresouza@gmail.com>
2018-05-09 23:36:41 -0300

    Adding level objects

--- FGA - The Endless Game/src/game/LevelMDS.java
+++ FGA - The Endless Game/src/game/LevelMDS.java
@@ -13,7 +13,7 @@ public class LevelMDS extends Level {
 		this.background = new GameImage("src//recursos//sprite//ground_mds.png");
 		addPlayer(300,300,"src//recursos/sprite//jogador2.png", 20, window);
 		this.initializeKeyboard();
-		this.initializeWalls();
+		this.initializeObjects();
 		
 	}
 	
@@ -55,10 +55,13 @@ public class LevelMDS extends Level {
 	}
 	
 	
-	private void initializeWalls() {
+	private void initializeObjects() {
 		
 		String horizontalWall = "src//recursos//sprite//horizontal_wall.png";
 		String verticalWall = "src//recursos//sprite//vertical_wall.png";
+		String mp_tv = "src//recursos//sprite//mocap_tv.png";
+		String mp_border_tv = "src//recursos//sprite//mocap_border_tv.png";
+		String mp_table = "src//recursos//sprite//mocap_table.png";
 		
 		//Parede superiores
 		GameObject parede1 = new GameObject(0, 0, horizontalWall, 1, GameObjectType.WALL);
@@ -66,11 +69,36 @@ public class LevelMDS extends Level {
 		GameObject parede3 = new GameObject(0, 25, verticalWall, 1, GameObjectType.WALL);
 		GameObject parede4 = new GameObject(775, 25, verticalWall, 1, GameObjectType.WALL);
 
+		//Tv
+		GameObject mocap_tv = new GameObject(250, 25, mp_tv, 1, GameObjectType.WALL);
+		GameObject mocap_border_tv = new GameObject(220, 25, mp_border_tv, 1, GameObjectType.WALL);
+		
+		//Mesas da esquerda
+		GameObject mocap_table1 = new GameObject(99, 150, mp_table, 1, GameObjectType.WALL);
+		GameObject mocap_table2 = new GameObject(99, 250, mp_table, 1, GameObjectType.WALL);
+		GameObject mocap_table3 = new GameObject(99, 350, mp_table, 1, GameObjectType.WALL);
+		GameObject mocap_table4 = new GameObject(99, 450, mp_table, 1, GameObjectType.WALL);
+		
+		//Mesas da direita
+		GameObject mocap_table5 = new GameObject(500, 150, mp_table, 1, GameObjectType.WALL);
+		GameObject mocap_table6 = new GameObject(500, 250, mp_table, 1, GameObjectType.WALL);
+		GameObject mocap_table7 = new GameObject(500, 350, mp_table, 1, GameObjectType.WALL);
+		GameObject mocap_table8 = new GameObject(500, 450, mp_table, 1, GameObjectType.WALL);
 		
 		this.addSceneObjects(parede1);
 		this.addSceneObjects(parede2);
 		this.addSceneObjects(parede3);
 		this.addSceneObjects(parede4);
+		this.addSceneObjects(mocap_tv);
+		this.addSceneObjects(mocap_border_tv);
+		this.addSceneObjects(mocap_table1);
+		this.addSceneObjects(mocap_table2);
+		this.addSceneObjects(mocap_table3);
+		this.addSceneObjects(mocap_table4);
+		this.addSceneObjects(mocap_table5);
+		this.addSceneObjects(mocap_table6);
+		this.addSceneObjects(mocap_table7);
+		this.addSceneObjects(mocap_table8);
 
 		
 	}
